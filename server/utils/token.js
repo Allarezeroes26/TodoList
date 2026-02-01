07fe0c58b04c4d1a86baef195708d20ea6e30979
Allarezeroes26 <johnerwibacani26@gmail.com>
2026-02-01 13:57:33 +0800

    render

--- server/utils/token.js
+++ server/utils/token.js
@@ -8,8 +8,8 @@ const generateToken = (userId, res) => {
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: 'lax',
-        secure: false
+        sameSite: 'none',
+        secure: true
     })
 }
 
