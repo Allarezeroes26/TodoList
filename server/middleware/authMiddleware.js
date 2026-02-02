21d415bd8b6085ec05d25ddaea5f4974542e35f2
Allarezeroes26 <johnerwibacani26@gmail.com>
2026-02-02 15:44:27 +0800

    spaces for readability

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,19 +4,15 @@ const User = require('../models/userModel')
 const authUpdate = async (req, res, next) => {
     try {
         const token = req.cookies.token
-
         if (!token) return res.status(401).json({ success: false, message: 'No token provided' })
 
         const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
-
         if (!decoded) return res.status(401).json({ success: false, message: "Token not valid" })
 
         const user = await User.findById( decoded.userId ).select('-password')
-
         if (!user) return res.status(401).json({ success: false, message: "User not Found" })
 
         req.user = user
-
         next()
 
     } catch (err) {
